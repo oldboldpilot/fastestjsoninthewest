3620faee22857457da4da5df29bb55a527ab4b34
oldboldpilot <oldboldpilot@github.com>
2025-11-27 01:42:31 -0800

    chore: Remove folders and files not in sync script
    - Removed ai/ and docs/ folders (not in FOLDERS_TO_SYNC)
    - Removed valgrind_results/ folder
    - Removed root files not in FILES_TO_SYNC list
    - Cleaned up development artifacts

---
+++ python_bindings/build/CMakeFiles/fastjson.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fastjson.dir/link.d"
+  "CMakeFiles/fastjson.dir/src/fastjson_bindings.cpp.o"
+  "CMakeFiles/fastjson.dir/src/fastjson_bindings.cpp.o.d"
+  "lib/fastjson.cpython-313-x86_64-linux-gnu.so"
+  "lib/fastjson.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fastjson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ python_bindings/build/CMakeFiles/fastjson.dir/link.d
@@ -0,0 +1,97 @@
+lib/fastjson.cpython-313-x86_64-linux-gnu.so: \
+  /lib/x86_64-linux-gnu/crti.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/crtbeginS.o \
+  CMakeFiles/fastjson.dir/src/fastjson_bindings.cpp.o \
+  /opt/clang-21/lib/x86_64-unknown-linux-gnu/libomp.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libstdc++.so \
+  /lib/x86_64-linux-gnu/libm.so \
+  /lib/x86_64-linux-gnu/libm.so \
+  /lib/x86_64-linux-gnu/libm.so \
+  /lib/x86_64-linux-gnu/libm.so.6 \
+  /lib/x86_64-linux-gnu/libmvec.so.1 \
+  /home/muyiwa/toolchain/clang-21/lib/libomp.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /lib/x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /lib/x86_64-linux-gnu/libpthread.a \
+  /lib/x86_64-linux-gnu/libc.so \
+  /lib/x86_64-linux-gnu/libc.so \
+  /lib/x86_64-linux-gnu/libc.so \
+  /lib/x86_64-linux-gnu/libc.so.6 \
+  /usr/lib/x86_64-linux-gnu/libc_nonshared.a \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /lib/x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/crtendS.o \
+  /lib/x86_64-linux-gnu/crtn.o
+
+/lib/x86_64-linux-gnu/crti.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/crtbeginS.o:
+
+CMakeFiles/fastjson.dir/src/fastjson_bindings.cpp.o:
+
+/opt/clang-21/lib/x86_64-unknown-linux-gnu/libomp.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libstdc++.so:
+
+/lib/x86_64-linux-gnu/libm.so:
+
+/lib/x86_64-linux-gnu/libm.so:
+
+/lib/x86_64-linux-gnu/libm.so:
+
+/lib/x86_64-linux-gnu/libm.so.6:
+
+/lib/x86_64-linux-gnu/libmvec.so.1:
+
+/home/muyiwa/toolchain/clang-21/lib/libomp.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/lib/x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/lib/x86_64-linux-gnu/libpthread.a:
+
+/lib/x86_64-linux-gnu/libc.so:
+
+/lib/x86_64-linux-gnu/libc.so:
+
+/lib/x86_64-linux-gnu/libc.so:
+
+/lib/x86_64-linux-gnu/libc.so.6:
+
+/usr/lib/x86_64-linux-gnu/libc_nonshared.a:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/lib/x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/crtendS.o:
+
+/lib/x86_64-linux-gnu/crtn.o:
